4722335c71643b80f68c4a202e17995ba9d2e517
Rakanrkk <19210134@njnu.edu.cn>
2022-03-26 22:40:22 +0800

    20220326seccompetition

--- jsy/20220326/20220326-5-1.cpp
+++ jsy/20220326/20220326-5-1.cpp
@@ -1,43 +1,10 @@
 //
 // Created by Rakan on 2022/3/26.
 //
-
 #include <iostream>
-#include <cstring>
-#include <vector>
-#include <functional>
-#include <algorithm>
-#include <cstdio>
-#include <cmath>
-#include <queue>
-#include <stack>
-#include <cstdlib>
-#include <ctime>
-#include <string>
-#include <map>
-#include <iomanip>
-#include <set>
-#include <deque>
+
 using namespace std;
-#define ifor(i, a, b) for ( int i = (a); i < (b); ++i)
-#define MEMS(seq, val) memset(seq, val, sizeof(seq));
-const int INF = 0x3f3f3f3f;
-typedef vector<int> Iv;
-typedef long long LL;
-typedef unsigned long long ULL;
-bool ifwmpf(LL n)
-{
-    while(n)
-    {
-        if( n%10!=1 && n%10!=4 && n%10!=9 && n%10!=0 )
-        {
-            return 0;
-        }
-        n/=10;
-    }
-    return 1;
-}
-LL sheet[1001]={0,
+long long sheet[1001]={0,
                 0,
                 1,
                 4,
@@ -1037,22 +1004,10 @@ LL sheet[1001]={0,
                 999000449900010000,
                 999014101119011041,
                 999099104994401401,
-                999190449909190449,};
-
+                999190449909190449};
 int main()
 {
-    int n=1;
-    LL temp;
-    for(LL i=0;n<1001;i++)
-    {
-        temp=i*i;
-        if (ifwmpf(temp))
-        {
-            sheet[n] = temp;
-            n++;
-            cout<<temp<<","<<endl;
-        }
-    }
-
-    return 0;
+    int i;
+    cin>>i;
+    cout<<sheet[i];
 }
