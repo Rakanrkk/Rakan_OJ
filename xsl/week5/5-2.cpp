dd38cd586886b2295fa582c7f6e8c62215cd108f
Rakanrkk <19210134@njnu.edu.cn>
2022-04-29 01:13:43 +0800

    week5

--- xsl/week5/5-2.cpp
+++ xsl/week5/5-2.cpp
@@ -2,3 +2,28 @@
 // Created by Rakan on 2022/4/29.
 //
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    int tmp;
+    vector<int> data;
+    int cnt=0;
+    for (int i = 0; i < n; i++) {
+        cin >> tmp;
+        data.push_back(tmp);
+    }
+    for (int i = 0; i < data.size(); i++) {
+        while (data[i] != i + 1) {
+            swap(data[i], data[data[i]-1]);
+            cnt++;
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
