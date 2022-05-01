e8399f3c4d9e5a3928041d0dc86228c07ea2c5a8
Rakanrkk <19210134@njnu.edu.cn>
2022-05-01 15:40:33 +0800

    week5

---
+++ newcoder/20220430/1-D.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Rakan on 2022/4/30.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    return 0;
+}
---
+++ newcoder/20220430/1-G.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Rakan on 2022/4/30.
+//
+
+// author:shujakuin  2022-04-30  12:22:11
+#include <iostream>
+using namespace std;
+int main()
+{
+
+
+
+
+
+
+
+    long long mod =  1000000007;
+    long long n;
+    cin >> n;
+//    // V-E+F=2
+//    // F = 2+E-V % (mod)
+//    long long ans=0;
+//    long long tmp = n*(n-1);
+//    tmp%=mod;
+//    long long tmp1 = (n-2)*(n-3);
+//    tmp1%=mod;
+//
+//    ans += (tmp)/2;
+//    ans%=mod;
+//    ans +=tmp*tmp1/24;
+//    ans%=mod;
+//    ans+=-n+1 ;
+//    ans = (ans+mod)%mod;
+//    cout << ans;
+//    return 0;
+
+}
